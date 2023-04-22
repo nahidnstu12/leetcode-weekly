dc6918fea4718290ba44a296b09f592b4537de7c
nahid_dev <mazharul.islam.nahid@softbdltd.com>
2023-04-22 14:06:36 +0600

    optimized 682, removing totalpoint var, use reduce for sum

--- nahid/682.js
+++ nahid/682.js
@@ -17,35 +17,35 @@
 var calPoints = function (operations) {
     let sum = [];
     let point = 0;
-    let totalPoint = 0
+    // let totalPoint = 0
     operations.forEach((item, index)=> {
         if(typeof +item && !isNaN(item)){
 
             // point = index == 0 ? +item : point + (+item);
              sum.push(+item)
-            totalPoint += (+item);
-            console.log("number", sum, totalPoint)
+            // totalPoint += (+item);
+            // console.log("number", sum, totalPoint)
         }
         else if(item == "C"){
-            // sum -=operations[index-1]
-            totalPoint -= sum.pop();
-            console.log("clear", sum, totalPoint)
+            sum.pop();
+            // totalPoint -= sum.pop();
+            // console.log("clear", sum, totalPoint)
         }
         else if(item == "D"){
             point = sum[sum.length - 1] * 2;
             sum.push(point)
-            totalPoint += point;
-            console.log("double", sum, totalPoint)
+            // totalPoint += point;
+            // console.log("double", sum, totalPoint)
         }else if(item == "+"){
             point = sum[sum.length - 1] + sum[sum.length - 2]
             sum.push(point)
-            totalPoint += point;
-            console.log("add", sum, totalPoint)
+            // totalPoint += point;
+            // console.log("add", sum, totalPoint)
         }
 
 
     })
-    return totalPoint;
+    return sum.reduce((acc, cur)=> acc+cur, 0);
 };
 
 const ops = ["5","-2","4","C","D","9","+","+"]
