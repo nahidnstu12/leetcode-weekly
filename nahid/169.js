6da96e1628922cd9c0f2cf09112a1b665e787b24
nahid_dev <mazharul.islam.nahid@softbdltd.com>
2023-04-23 22:42:50 +0600

    solve 169

--- nahid/169.js
+++ nahid/169.js
@@ -4,22 +4,38 @@
  * Input: nums = [2,2,1,1,1,2,2]
  * Output: 2
  */
-var majorityElement = function (nums) {
-  const obj = nums.reduce((acc, cur) => {
-    if (acc[cur]) {
-      acc[cur]++;
-    } else {
-      acc[cur] = 1;
-    }
-    return acc;
-  }, {});
-  const max = Math.max(...Object.values(obj));
-  for (const objKey in obj) {
-    if (obj[objKey] === max) {
-      return Number(objKey);
-    }
-  }
-  // console.log(keys, vals, max);
-};
+// var majorityElement = function (nums) {
+//   const obj = nums.reduce((acc, cur) => {
+//     if (acc[cur]) {
+//       acc[cur]++;
+//     } else {
+//       acc[cur] = 1;
+//     }
+//     return acc;
+//   }, {});
+//   const max = Math.max(...Object.values(obj));
+//   for (const objKey in obj) {
+//     if (obj[objKey] === max) {
+//       return Number(objKey);
+//     }
+//   }
+//   // console.log(keys, vals, max);
+// };
 
-console.log(majorityElement([3, 2, 3]));
+// from leetcode solve(Boyer–Moore_majority_vote_algorithm)
+function majorityElement(nums) {
+  let candidate;
+  let count = 0;
+
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num; //2
+    }
+    count += num === candidate ? 1 : -1; //1
+    console.log("cad", candidate, count);
+  }
+
+  return candidate;
+}
+
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
