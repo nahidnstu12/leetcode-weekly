390418893b58d106d4837e946b58ff9b3d588264
nahidnstu12 <nahid.dev19@gmail.com>
2025-02-07 18:16:41 +0600

    neetcode 5

---
+++ arrays-hashing/238-product-of-array-except-self.js
@@ -0,0 +1,14 @@
+/**
+ * LeetCode #238 - Product of Array Except Self
+ * Category: ARRAY
+ * Difficulty: TODO: Add difficulty (Easy/Medium/Hard)
+ */
+
+
+function solution(nums) {
+    
+}
+
+// Example Usage
+console.log(solution([1,2,3,4])); 
+// [24,12,8,6]
--- arrays-hashing/347-top-k-frequent-elements.js
+++ arrays-hashing/347-top-k-frequent-elements.js
@@ -6,7 +6,7 @@
 
 
 function solution2(nums, k) {
-    if (nums.length == 1 && k == 1) return nums
+    if (nums.length == k) return nums
     const mapping = nums.reduce((acc, cur) => {
         if (acc[cur]) {
             acc[cur]++
@@ -34,36 +34,28 @@ function solution2(nums, k) {
 }
 
 function solution(nums, k) {
-    if (nums.length == 1 && k == 1) return nums
+    if (nums.length == k) return nums
     const heapMap = new Map();
     let arr = []
 
     nums.forEach(i => {
         if (heapMap.has(i)) {
-            // heapMap.get(i)[1]++;
             heapMap.set(i, [i, ++heapMap.get(i)[1]])
         } else {
             heapMap.set(i, [i, 1])
         }
     })
-    console.log(heapMap);
     for (let [i, val] of heapMap) {
-        
         if (arr.length < k) {
             // insert arr
-            arr.push(val)
-            bubbleUp(arr, arr.length - 1)
+            arr.push(val) //push end of arr
+            bubbleUp(arr, arr.length - 1)  // Move up if smaller
 
         } else if(val[1] > arr[0][1]) {
-            // pop element 
-            arr[0] = val;
-            bubbleDown(arr, 0)
-
-
+            arr[0] = val; // replace 0 element
+            bubbleDown(arr, 0)  //bubbleDown when replacing root, root required smallest always
         }
-
     }
-
 
     return arr.map(i => i[0])
 }
@@ -101,6 +93,6 @@ function bubbleDown(heap, index){
 }
 
 // Example Usage
-console.log(solution2([1, 1, 1, 2, 2, 3, 3, 3, 3, 4,4,5,5,2,2,2], 2));
-console.log(solution2([1,2], 2));
+console.log(solution([1, 1, 1, 2, 2, 3, 3, 3, 3, 4,4,5,5,2,2,2], 2));  // [2,3]
+// console.log(solution([1,2], 2));
 // Output: [1,3]
