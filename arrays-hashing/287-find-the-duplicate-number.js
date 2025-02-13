bd7d86bb7a02c8cdb684ff2e05dff657f07b402f
nahidnstu12 <nahid.dev19@gmail.com>
2025-02-13 18:04:48 +0600

    neetcode 8

---
+++ arrays-hashing/287-find-the-duplicate-number.js
@@ -0,0 +1,13 @@
+/**
+ * LeetCode #287 - Find the Duplicate Number
+ * Category: ARRAY
+ * Difficulty: TODO: Add difficulty (Easy/Medium/Hard)
+ */
+
+
+function solution() {
+    
+}
+
+// Example Usage
+console.log(solution()); 
--- arrays-hashing/535-encode-and-decode-tinyurl.js
+++ arrays-hashing/535-encode-and-decode-tinyurl.js
@@ -4,10 +4,49 @@
  * Difficulty: TODO: Add difficulty (Easy/Medium/Hard)
  */
 
+let urlMap = new Map();
+const base62str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-function solution() {
+var encode = function (longUrl) {
     
-}
+    let urlSum = 0;
+    let key = 0;
+    let primeNumber = 3
+    let tinyurl = ""
+    
+    // making sum 
+    for (let l of longUrl.slice(8)) {
+        
+        
+        urlSum = (l.charCodeAt() + urlSum* primeNumber) % Math.pow(10, 12)
+        // console.log(l.charCodeAt(), l, urlSum);
+    }
+    key = urlSum;
+    // convert base62
+    while (urlSum > 0) {
+
+        tinyurl += base62str.charAt(urlSum % 62)
+
+        urlSum = Math.floor(urlSum / 62);
+    }
+    urlMap.set(key, longUrl)
+    console.log(urlMap);
+    return tinyurl;
+};
+
+var decode = function (shortUrl) {
+    let sum = 0;
+    for(let i=0; i<shortUrl.length; i++){
+        
+        sum += base62str.indexOf(shortUrl[i]) * Math.pow(62, i)
+        
+    }
+    return urlMap.has(sum) ? urlMap.get(sum) : ""
+    
+};
 
 // Example Usage
-console.log(solution()); 
+
+const url = "https://bone.example.net/?brother=agreement&beds=bird"
+// const url = "https://leetcode.com/problems/design-tinyurl"
+console.log(decode(encode(url))); 
